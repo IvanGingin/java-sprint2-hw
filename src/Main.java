63e770fb5813b29bb7d35a53ce1afd7a76eb7a64
IvanGingin <ginginivan@gmail.com>
2023-11-11 12:00:17 +0700

    заготовка тела программы

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-        // Поехали!
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printMenu();
+            int command = scanner.nextInt();
+            if (command == 1) {
+                ReportEngine.readMonthlyReports();
+
+            } else if (command == 2) {
+                ReportEngine.readYearlyReports();
+
+            } else if (command == 3) {
+                ReportEngine.compareReports();
+
+            } else if (command == 4) {
+                ReportEngine.printMonthlyReports();
+
+            } else if (command == 5) {
+                ReportEngine.printYearlyReports();
+
+            } else if (command == 0) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Извините, такой команды пока нет.");
+            }
+        }
+    }
+    private static void printMenu() {
+        System.out.println("Выберите действие:");
+        System.out.println("1 - Считать все месячные отчёты");
+        System.out.println("2 - Считать годовой отчёт");
+        System.out.println("3 - Сверить отчёты");
+        System.out.println("4 - Вывести информацию обо всех месячных отчётах");
+        System.out.println("5 - Вывести информацию о годовом отчёте");
+        System.out.println("0 - Выход");
     }
 }
 
---
+++ src/ReportEngine.java
@@ -0,0 +1,17 @@
+public class ReportEngine {
+    public static void readMonthlyReports(){ //считывает месячные отчеты
+
+    }
+    public static void readYearlyReports() { //считывает годовые отчеты
+
+    }
+    public static void compareReports() { //сравнение отчетов
+
+    }
+    public static void printMonthlyReports() { // выводит месячные отчеты
+
+    }
+    public static void printYearlyReports() { // вывод годовых отчетов
+
+    }
+}
